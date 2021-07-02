5d315ce9edbd75ebde93dea7797354017139122e
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-07-02 19:29:57 +0530

    Update m(err) is nil

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -51,7 +51,7 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m != nil {
+		if m == nil {
 			http.NotFound(w, r)
 			return
 		}
