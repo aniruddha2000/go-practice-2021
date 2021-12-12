16451cd54559de17cd51dee3d6592fa941ce6111
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-12-12 22:02:57 +0530

    Update hello code

---
+++ hello/hello_var.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 3
+	fmt.Println(a)
+	a = "qa"
+	fmt.Println(a)
+
+	var b string
+	b = "ani"
+	fmt.Println(b)
+	var b int
+	b = 6
+	fmt.Println(b)
+}
