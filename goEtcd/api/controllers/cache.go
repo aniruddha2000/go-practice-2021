4dbc869b8a177267b8b31e8f638a0c2a188bcd9a
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-04-19 20:41:01 +0530

    Replace the mux functions with http functions
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- goEtcd/api/controllers/cache.go
+++ goEtcd/api/controllers/cache.go
@@ -1,47 +1,73 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	j "github.com/aniruddha2000/goEtcd/api/json"
-	"github.com/gorilla/mux"
 )
 
 func (s *Server) Create(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	key := r.Form["key"]
-	val := r.Form["val"]
+	if r.Method == "POST" {
+		r.ParseForm()
+		key := r.Form["key"]
+		val := r.Form["val"]
 
-	for i := 0; i < len(key); i++ {
-		s.Cache.Store(key[i], val[i])
+		for i := 0; i < len(key); i++ {
+			s.Cache.Store(key[i], val[i])
+		}
+
+		j.JSON(w, r, http.StatusCreated, "Record created")
+	} else {
+		j.JSON(w, r, http.StatusBadRequest, "POST Request accepted")
 	}
-
-	j.JSON(w, r, http.StatusCreated, "Record created")
 }
 
 func (s *Server) List(w http.ResponseWriter, r *http.Request) {
-	records := s.Cache.List()
-	j.JSON(w, r, http.StatusOK, records)
+	if r.Method == "GET" {
+		records := s.Cache.List()
+		j.JSON(w, r, http.StatusOK, records)
+	} else {
+		j.JSON(w, r, http.StatusBadRequest, "GET Request accepted")
+	}
 }
 
 func (s *Server) Get(w http.ResponseWriter, r *http.Request) {
-	key := mux.Vars(r)["key"]
+	if r.Method == "GET" {
+		keys, ok := r.URL.Query()["key"]
+		if !ok || len(keys[0]) < 1 {
+			log.Println("Url Param 'key' is missing")
+			return
+		}
+		key := keys[0]
 
-	val, err := s.Cache.Get(key)
-	if err != nil {
-		j.JSON(w, r, http.StatusNotFound, err.Error())
-		return
+		val, err := s.Cache.Get(key)
+		if err != nil {
+			j.JSON(w, r, http.StatusNotFound, err.Error())
+			return
+		}
+		j.JSON(w, r, http.StatusOK, map[string]string{key: val})
+	} else {
+		j.JSON(w, r, http.StatusBadRequest, "POST Request accepted")
 	}
-	j.JSON(w, r, http.StatusOK, map[string]string{key: val})
 }
 
 func (s *Server) Delete(w http.ResponseWriter, r *http.Request) {
-	key := mux.Vars(r)["key"]
+	if r.Method == "DELETE" {
+		keys, ok := r.URL.Query()["key"]
+		if !ok || len(keys[0]) < 1 {
+			log.Println("Url Param 'key' is missing")
+			return
+		}
+		key := keys[0]
 
-	err := s.Cache.Delete(key)
-	if err != nil {
-		j.JSON(w, r, http.StatusNotFound, err.Error())
-		return
+		err := s.Cache.Delete(key)
+		if err != nil {
+			j.JSON(w, r, http.StatusNotFound, err.Error())
+			return
+		}
+		j.JSON(w, r, http.StatusNoContent, map[string]string{"data": "delete"})
+	} else {
+		j.JSON(w, r, http.StatusBadRequest, "DELETE Request accepted")
 	}
-	j.JSON(w, r, http.StatusNoContent, map[string]string{"data": "delete"})
 }
