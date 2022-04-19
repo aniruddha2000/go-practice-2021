53ba51549ee2c7561cb17bac9154e473d015f763
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-04-19 20:41:29 +0530

    Modified routes with http conventions
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- goEtcd/api/controllers/routes.go
+++ goEtcd/api/controllers/routes.go
@@ -1,8 +1,8 @@
 package controllers
 
 func (s *Server) initializeRoutes() {
-	s.Router.HandleFunc("/record", s.Create).Methods("POST")
-	s.Router.HandleFunc("/record", s.List).Methods("GET")
-	s.Router.HandleFunc("/record/{key}", s.Get).Methods("GET")
-	s.Router.HandleFunc("/record/{key}", s.Delete).Methods("DELETE")
+	s.Router.HandleFunc("/record", s.Create)
+	s.Router.HandleFunc("/records", s.List)
+	s.Router.HandleFunc("/get/record", s.Get)
+	s.Router.HandleFunc("/del/record", s.Delete)
 }
