d225e85d43e2706f517e8ce60e9d624b5bc67bb1
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-04-20 22:58:33 +0530

    Add ReadFile Exists DirExist utilities
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- goEtcd/filesystem/utils.go
+++ goEtcd/filesystem/utils.go
@@ -1,6 +1,8 @@
 package filesystem
 
-import "os"
+import (
+	"os"
+)
 
 func DirExists(fs Fs, name string) (bool, error) {
 	file, err := fs.Stat(name)
@@ -11,4 +13,41 @@ func DirExists(fs Fs, name string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func Exists(fs Fs, name string) (bool, error) {
+	_, err := fs.Stat(name)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func ReadDir(fs Fs, dirName string) ([]os.FileInfo, error) {
+	dir, err := fs.Open(dirName)
+	if err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+	list, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
+
+func ReadFile(fs Fs, name string) ([]byte, error) {
+	file, err := fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
